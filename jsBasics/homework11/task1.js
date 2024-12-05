d9c16a361193564bbed06098d31e6d46d745d191
Dima Tkachenko <Demitry.Tkachenko@gmail.com>
2024-12-05 12:32:22 +0200

    Test husky and eslint

--- jsBasics/homework11/task1.js
+++ jsBasics/homework11/task1.js
@@ -1,13 +1,13 @@
-import { faker } from "@faker-js/faker";  
-  
-const randomTimeout = faker.number.int({min: 1000, max: 5000});
-const randomText = faker.lorem.words();
+import { faker } from "@faker-js/faker"
+
+const randomTimeout = faker.number.int({min: 1000, max: 5000})
+const randomText = faker.lorem.word()
 
 function printWithTimeout(text, timeout) {
   setTimeout(() => {
-    console.log(text);
-  }, timeout);
+    console.log(text)
+  }, timeout)
 }
 
-printWithTimeout(randomText, randomTimeout);    
-console.log(`Total wait time: ${randomTimeout} ms`);
+printWithTimeout(randomText, randomTimeout)    
+console.log(`Total wait time: ${randomTimeout} ms`)
