c52d1ffaf3868529ce72da3536986279ba5ca062
Dima Tkachenko <Demitry.Tkachenko@gmail.com>
2024-12-05 12:59:31 +0200

    test husky

--- jsBasics/homework11/task1.js
+++ jsBasics/homework11/task1.js
@@ -1,13 +1,15 @@
 import { faker } from "@faker-js/faker"
 
-const randomTimeout = faker.number.int({min: 1000, max: 5000})
-const randomText = faker.lorem.word()
+const randomTimeout = faker.number.int({min: 1000, max: 5000}) 
+const randomText = faker.lorem.words() 
 
 function printWithTimeout(text, timeout) {
   setTimeout(() => {
     console.log(text)
-  }, timeout)
+  }, timeout) 
 }
+
+const unusedVariable = "This variable is not used"
 
 printWithTimeout(randomText, randomTimeout)    
 console.log(`Total wait time: ${randomTimeout} ms`)
