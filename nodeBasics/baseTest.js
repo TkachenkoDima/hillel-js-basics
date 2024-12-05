3f68bb5d3295bc56d1a92b321d373b703fa22779
Dima Tkachenko <Demitry.Tkachenko@gmail.com>
2024-12-05 12:55:38 +0200

    test husky

--- nodeBasics/baseTest.js
+++ nodeBasics/baseTest.js
@@ -5,7 +5,7 @@ const lastName = 'Ololoev';
 let age = 11;
 console.log(chalk.green(`Hello, my name is ${firstName} ${lastName} and I am ${age} years old.`));
 
-console.log(chalk.blue(firstName ));
+console.log(chalk.blue(firstName));
 console.log(chalk.red(lastName));
 console.log(chalk.yellow(age));
 
