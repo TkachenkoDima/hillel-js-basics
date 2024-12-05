ea365bea88061ad5141f61f332eb7c16b03acb9a
Dima Tkachenko <Demitry.Tkachenko@gmail.com>
2024-12-05 13:01:48 +0200

    test husky

--- jsBasics/homework11/task1.js
+++ jsBasics/homework11/task1.js
@@ -1,15 +1,16 @@
-import { faker } from "@faker-js/faker"
+import { faker } from "@faker-js/faker";  
+  
+const randomTimeout = faker.number.int({min: 1000, max: 5000});
 
-const randomTimeout = faker.number.int({min: 1000, max: 5000}) 
-const randomText = faker.lorem.words() 
+const randomText = faker.lorem.words();
 
 function printWithTimeout(text, timeout) {
+
   setTimeout(() => {
-    console.log(text)
-  }, timeout) 
+    console.log(text);
+  }, timeout)
+
 }
 
-const unusedVariable = "This variable is not used"
-
-printWithTimeout(randomText, randomTimeout)    
-console.log(`Total wait time: ${randomTimeout} ms`)
+printWithTimeout(randomText, randomTimeout);    
+console.log(`Total wait time: ${randomTimeout} ms`);
